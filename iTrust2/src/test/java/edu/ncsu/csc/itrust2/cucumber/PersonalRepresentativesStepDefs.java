5503d9eefc3ee3e0ddd1d54df451668ffa4e91d2
jrstruck <jrstruck@ncsu.edu>
2018-04-05 12:59:20 -0400

    Increased sleep times to make tests more reliable for cucumber
    CLoses #41

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/PersonalRepresentativesStepDefs.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/PersonalRepresentativesStepDefs.java
@@ -208,7 +208,7 @@ public class PersonalRepresentativesStepDefs {
      * @throws InterruptedException
      */
     private void pause () throws InterruptedException {
-        Thread.sleep( 500 );
+        Thread.sleep( 2000 );
     }
 
     /**
